c7d50d96b9df97a3ed0318ad377092eda3298bfd
ArthurMaciel95 <arthurnmrocha@gmail.com>
2021-11-26 15:57:58 -0300

    :fire: commit to push

--- 4_value_zero/main.go
+++ 4_value_zero/main.go
@@ -5,10 +5,12 @@ import "fmt"
 var name string
 var age int
 var temperature float32
+var isNull bool
 
 func main() {
 
 	fmt.Printf("%v, %T\n", name, name)
 	fmt.Printf("%v, %T\n", age, age)
 	fmt.Printf("%v, %T\n", temperature, temperature)
+	fmt.Printf("%v, %T\n", isNull, isNull)
 }
---
+++ 5_package_fmt/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// raw string
+	grettings := `oi bom dia\ncomo vai?\nespero que tudo bem.`
+
+	// literais string
+	car := "tesla\nmodelo x3"
+
+	fmt.Println(grettings)
+	fmt.Println(car)
+}
